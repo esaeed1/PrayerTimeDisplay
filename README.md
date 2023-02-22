c087dff1e0f0e34300023c725e9b5415b5af512f
Edrees <edrees@wayne.edi>
2023-02-22 16:58:04 -0500

    added feature to view the weather problem to fix is the tempature displays Â that should not be displayed

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Prayer Time Display
 
 Using this to calculate hijri date 
 https://www.al-habib.info/islamic-calendar/hijricalendartext.htm
+
+Using this API to display the weather
+https://weather-gov.github.io/api/gridpoints
--- js/script.js
+++ js/script.js
@@ -27,3 +27,33 @@ settingsIcon.addEventListener('click', function () {
         settingsContainer.style.display = 'none';
     }
 });
+
+const toggleWeatherInfo = document.getElementById('toggle-weather-info');
+const weatherInfo = document.getElementById('weather-info');
+
+toggleWeatherInfo.addEventListener('click', () => {
+    if (toggleWeatherInfo.checked) {
+        // Make a request to the weather-gov API and display the weather info
+        fetch('https://api.weather.gov/gridpoints/DTX/65,79/forecast/hourly')
+            .then(response => response.json())
+            .then(data => {
+                const temperature = data.properties.periods[0].temperature;
+                const windSpeed = data.properties.periods[0].windSpeed;
+                const temperatureElement = document.getElementById('temperature');
+                const windSpeedElement = document.getElementById('wind-speed');
+
+                temperatureElement.textContent = temperature + ' °F';
+                windSpeedElement.textContent = windSpeed;
+            })
+            .catch(error => {
+                console.log('Error:', error);
+            });
+
+        // Show the weather info
+        weatherInfo.style.display = 'block';
+    } else {
+        // Hide the weather info
+        weatherInfo.style.display = 'none';
+    }
+});
+
