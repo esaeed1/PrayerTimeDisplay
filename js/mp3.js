d0a9be9294a82238bdbf2b447bdf4dca5db20fbf
esaeed1 <117033347+esaeed1@users.noreply.github.com>
2023-04-08 13:36:58 -0400

    wrote the mp3 file in cleaner way

--- js/mp3.js
+++ js/mp3.js
@@ -1,50 +1,29 @@
-const fajrTimeCell2 = document.getElementById('fajr-time');
-const dhuhrTimeCell2 = document.getElementById('dhuhr-time');
-const asrTimeCell2 = document.getElementById('asr-time');
-const maghribTimeCell2 = document.getElementById('maghrib-time');
-const ishaTimeCell2 = document.getElementById('isha-time');
+const prayerTimes = [
+    { name: 'fajr', cell: document.getElementById('fajr-time'), adhan: new Audio('/mp3/Fajr-Mishary-Rashid-Alafasy.mp3') },
+    { name: 'dhuhr', cell: document.getElementById('dhuhr-time'), adhan: new Audio('/mp3/Adhan-Mishary-Rashid-Alafasy.mp3') },
+    { name: 'asr', cell: document.getElementById('asr-time'), adhan: new Audio('/mp3/Adhan-Mishary-Rashid-Alafasy.mp3') },
+    { name: 'maghrib', cell: document.getElementById('maghrib-time'), adhan: new Audio('/mp3/Adhan-Mishary-Rashid-Alafasy.mp3') },
+    { name: 'isha', cell: document.getElementById('isha-time'), adhan: new Audio('/mp3/Adhan-Mishary-Rashid-Alafasy.mp3') }
+];
 
-// Create audio objects for the adhan sounds
-const fajrAdhan = new Audio('/mp3/Fajr-Mishary-Rashid-Alafasy.mp3');
-const regularAdhan = new Audio('/mp3/Adhan-Mishary-Rashid-Alafasy.mp3');
-
-function updateTimes() {
+function checkAdhan(prayer) {
     const now = new Date();
     const currentHour = hours12(now);
     const currentMinute = now.getMinutes();
     const currentPeriod = now.getHours() < 12 ? "AM" : "PM";
 
+    const time = prayer.cell.textContent;
+    const period = time.split(' ')[1];
+    const hour = parseInt(time.split(':')[0]);
+    const minute = parseInt(time.split(':')[1]);
 
-    const fajrTime = fajrTimeCell2.textContent;
-    const fajrPeriod = fajrTime.split(' ')[1];
-    if (currentHour === parseInt(fajrTime.split(':')[0]) && currentMinute === parseInt(fajrTime.split(':')[1]) && currentPeriod === fajrPeriod) {
-        fajrAdhan.play();
+    if (currentHour === hour && currentMinute === minute && currentPeriod === period) {
+        prayer.adhan.play();
     }
+}
 
-    const dhuhrTime = dhuhrTimeCell2.textContent;
-    const dhuhrPeriod = dhuhrTime.split(' ')[1];
-    if (currentHour === parseInt(dhuhrTime.split(':')[0]) && currentMinute === parseInt(dhuhrTime.split(':')[1]) && currentPeriod === dhuhrPeriod) {
-        regularAdhan.play();
-    }
-
-    const asrTime = asrTimeCell2.textContent;
-    const asrPeriod = asrTime.split(' ')[1];
-    if (currentHour === parseInt(asrTime.split(':')[0]) && currentMinute === parseInt(asrTime.split(':')[1]) && currentPeriod === asrPeriod) {
-        regularAdhan.play();
-    }
-
-    const maghribTime = maghribTimeCell2.textContent;
-    const maghribPeriod = maghribTime.split(' ')[1];
-    if (currentHour === parseInt(maghribTime.split(':')[0]) && currentMinute === parseInt(maghribTime.split(':')[1]) && currentPeriod === maghribPeriod) {
-        regularAdhan.play();
-    }
-
-    const ishaTime = ishaTimeCell2.textContent;
-    const ishaPeriod = ishaTime.split(' ')[1];
-    if (currentHour === parseInt(ishaTime.split(':')[0]) && currentMinute === parseInt(ishaTime.split(':')[1]) && currentPeriod === ishaPeriod) {
-        regularAdhan.play();
-    }
-
+function updateTimes() {
+    prayerTimes.forEach(checkAdhan);
 }
 
 function hours12(date) {
